08eca529980f7f44d3d7697f8fe61ad6f667b2ca
Fernando <feromes@gmail.com>
2011-12-01 13:08:17 -0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContaMobi::Application.config.secret_token = '6801e82ee47cc62cd5e6ae8027875ce6ca106a41d00a9b070d589a2d810905bb5a9f03097b7ca547b71287b29422ac581ca6b86df42e75870e7a69725dfcf95f'
