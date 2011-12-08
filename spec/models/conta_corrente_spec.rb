6cff764171cd0a9f9f876480fe8afeffa65a5d33
Fernando <feromes@gmail.com>
2011-12-08 14:42:35 -0200

    Specs da Conta Corrente

--- spec/models/conta_corrente_spec.rb
+++ spec/models/conta_corrente_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
-describe ContaCorrente do
+describe ContaCorrente do 
+  context "Dada uma conta corrente criada com saldo zerado" do
+     it "Deve poder depositar inicialmente R$ 100,00" do
+       
+     end
+     it "Deve sacar R$ 60,00 e o saldo agora terá que ser R$ 40,00" do
+       
+     end
+  end
   
+  context "Dada uma conta corrente criada com saldo inicial de R$ 899,00" do
+     it "Deve poder sacar R$ 899,00 e o saldo ficar zerado" do
+       
+     end
+  end
 end
