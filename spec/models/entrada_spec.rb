beebf36fa6fdffb9f7328bb1431081c628d151a4
Fernando <feromes@gmail.com>
2011-12-09 16:53:09 -0200

    mais specs, e testes

--- spec/models/entrada_spec.rb
+++ spec/models/entrada_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 
 describe Entrada do
   context "Dada uma conta de entrada Salario com lançamentos que somam R$ 100.000" do
+    entrada_salario = Entrada.create(nome:"Salario")
+    10.times { entrada_salario.novo_lancamento(10000.00) }
+    it "um saldo de R$ 100000.00 deve estar disponível" do
+      entrada_salario.saldo.should eq(100000.0) 
+    end
     it "uma nova sub conta de salario, cahamada Horas Extras é criada" do
       pending
     end
--- spec/models/razao_spec.rb
+++ spec/models/razao_spec.rb
@@ -14,8 +14,10 @@ describe Razao do
   end
   
   context "Dada uma conta de entrada chamada Recebimento de Trabalhos extras" do
+    recebimento_de_trabalhos_extras = Entrada.create(nome: "Recebimento de trabalhos extras")
     it "um novo lancamento no valor de R$ 1000,00 é realizado" do
-      pending("get the vocal chords working")
+      recebimento_de_trabalhos_extras.novo_lancamento(1000.00)
+      recebimento_de_trabalhos_extras.saldo.should eq(1000.00)
     end
   end
 end
